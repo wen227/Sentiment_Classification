b09d8e96af3da104a92cd9a8b76d6945effbb49a
wen227 <45067258+wen227@users.noreply.github.com>
2019-12-01 21:03:37 +0800

    Update sentiment_classification.py

--- sentiment_classification.py
+++ sentiment_classification.py
@@ -5,7 +5,7 @@ author:wen227
 github:https://github.com/wen227/Sentiment_Classification
 reference:
 1.https://machinelearningmastery.com/sequence-classification-lstm-recurrent-neural-networks-python-keras/
-  This page presents a simple example of LSTM neural network model
+  This web presents a simple example of LSTM neural network model
 2.https://zhuanlan.zhihu.com/p/49271699 This article tells the history of word embedding
 3.https://my.oschina.net/u/3800567/blog/2887156 This article shows the way to plot results
 In order to set hyperparameters ,I also look for many articles which are listed in my experimental report.
